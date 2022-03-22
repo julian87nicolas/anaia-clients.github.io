a42d0c5c3e62b30e16e008caf63daf645b27b3cd
julian87nicolas <julicmrgo@gmail.com>
2022-03-22 17:48:24 -0300

    test import

--- scripts/inner.js
+++ scripts/inner.js
@@ -1,7 +1,11 @@
 
 const test = document.querySelector('.test');
 
-const jsonData= require('./GrillaCliente.json'); 
+const jsonData= require('./GrillaCliente.json');
+import datimport from './GrillaCliente.json';
+
 console.log(jsonData);
 
 test.innerHTML = "<p>"+jsonData+"</p>";
+
+test.innerHTML = "<p>"+datimport+"</p>";
