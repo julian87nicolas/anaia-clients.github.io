ab20cf489992c3a590544c31504c1b09af2a5112
julian87nicolas <julicmrgo@gmail.com>
2022-03-22 17:43:34 -0300

    cleaned

--- scripts/inner.js
+++ scripts/inner.js
@@ -1,8 +1,7 @@
 
 const test = document.querySelector('.test');
-test.innerHTML = '<h1>Hello World</h1>';
 
 const jsonData= require('./GrillaCliente.json'); 
 console.log(jsonData);
 
-test.innerHTML = jsonData;
+test.innerHTML = "<p>"+jsonData+"</p>";
