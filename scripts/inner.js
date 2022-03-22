52363c4b9bb33ffe23daef9e79609d2ea035ecce
julian87nicolas <julicmrgo@gmail.com>
2022-03-22 18:01:32 -0300

    xlsx test

--- scripts/inner.js
+++ scripts/inner.js
@@ -1,7 +1,12 @@
-
+/*
 const test = document.querySelector('.test');
 
 const jsonData= require('./GrillaCliente.json');
 console.log(jsonData);
+*/
+
+const XLSX = require('xlsx');
+
+jsonData = XLSX.readFile('./GrillaCliente.xlsx');
 
 test.innerHTML = "<p>"+jsonData+"</p>";
