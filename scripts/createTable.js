ac772ddf67ae10474a3a3447b950cda527adb015
julian87nicolas <julicmrgo@gmail.com>
2022-04-01 17:02:23 -0300

    clients list from drive sheets

---
+++ scripts/createTable.js
@@ -0,0 +1,5 @@
+const mySpreadsheet = "https://docs.google.com/spreadsheets/d/1_DE59KOzqXEotnVPJ54XHpKFBlqDKtT8GoXy7nREY5o/edit#gid=366171760";
+
+$('#sheet').sheetrock({
+    url: mySpreadsheet,
+});
